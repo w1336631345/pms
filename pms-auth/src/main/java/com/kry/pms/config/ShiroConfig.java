f4d032b67b9e6c59945dd64cd30611e068e7982a
huanghaibin <631131196@qq.com>
2019-10-16 11:30:36 +0800

    过滤接口验证

--- pms-auth/src/main/java/com/kry/pms/config/ShiroConfig.java
+++ pms-auth/src/main/java/com/kry/pms/config/ShiroConfig.java
@@ -30,7 +30,7 @@ public class ShiroConfig {
         Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
         // <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
         filterChainDefinitionMap.put("/auth/admin/login", "anon");
-//        filterChainDefinitionMap.put("/api/**", "anon");
+        filterChainDefinitionMap.put("/api/**", "anon");
 
         filterChainDefinitionMap.put("/admin/**", "authc");
         filterChainDefinitionMap.put("/user/**", "authc");
