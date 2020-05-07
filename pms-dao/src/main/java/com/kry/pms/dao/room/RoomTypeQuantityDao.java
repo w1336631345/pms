76b1e725ed54e3fe2dc970fe14a8ca56d850a58c
Louis <louis_lw@126.com>
2020-05-07 17:57:23 +0800

    调整房类资源查询

--- pms-dao/src/main/java/com/kry/pms/dao/room/RoomTypeQuantityDao.java
+++ pms-dao/src/main/java/com/kry/pms/dao/room/RoomTypeQuantityDao.java
@@ -29,10 +29,10 @@ public interface RoomTypeQuantityDao extends BaseDao<RoomTypeQuantity> {
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	RoomTypeQuantity findByRoomTypeAndQuantityDate(RoomType roomType,LocalDate quantityDate);
 	
-	@Query(value = "select t.* from t_room_type_quantity t where t.room_type_id =?1 and t.quantity_date between ?2 and ?3 order by predictable_total asc limit 1", nativeQuery = true)
+	@Query(value = "select t.* from t_room_type_quantity t where t.room_type_id =?1 and t.quantity_date >= ?2 and t.quantity_date <= ?3 order by predictable_total asc limit 1", nativeQuery = true)
 	RoomTypeQuantity queryPredictable(String roomTypeId, LocalDate startDate, LocalDate endDate);
 	
-	@Query(value = "select 	t.* from t_room_type_quantity t where t.hotel_code =?1 and t.quantity_date between ?2 and ?3 ", nativeQuery = true)
+	@Query(value = "select 	t.* from t_room_type_quantity t where t.hotel_code =?1 and t.quantity_date >= ?2 and t.quantity_date <= ?3 ", nativeQuery = true)
 	List<RoomTypeQuantity> queryByDay(String currentHotleCode, LocalDate startDate, LocalDate endDate);
 
 	List<RoomTypeQuantity> findByHotelCodeAndQuantityDate(String hotleCode, LocalDate date);
