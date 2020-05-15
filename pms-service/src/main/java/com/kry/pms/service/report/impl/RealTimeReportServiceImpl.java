75d0a53e8a55bbc3631762539e6fff41465ba4ee
Louis <louis_lw@126.com>
2020-05-15 09:36:53 +0800

    调整报表

--- pms-service/src/main/java/com/kry/pms/service/report/impl/RealTimeReportServiceImpl.java
+++ pms-service/src/main/java/com/kry/pms/service/report/impl/RealTimeReportServiceImpl.java
@@ -1,7 +1,9 @@
 package com.kry.pms.service.report.impl;
 
 import com.kry.pms.service.report.RealTimeReportService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RealTimeReportServiceImpl implements RealTimeReportService {
 
 
