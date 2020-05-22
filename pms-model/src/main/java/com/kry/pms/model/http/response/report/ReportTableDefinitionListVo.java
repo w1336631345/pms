0d35574f0a89fbf7d9ce1ea42299de3317d32d29
Louis <louis_lw@126.com>
2020-05-22 15:17:13 +0800

    账号检查

--- pms-model/src/main/java/com/kry/pms/model/http/response/report/ReportTableDefinitionListVo.java
+++ pms-model/src/main/java/com/kry/pms/model/http/response/report/ReportTableDefinitionListVo.java
@@ -8,14 +8,16 @@ public class ReportTableDefinitionListVo {
     private String name;
     private String groupKey;
     private String type;
+    private String queryParams;
 
     public ReportTableDefinitionListVo() {
     }
 
-    public ReportTableDefinitionListVo(String id, String name, String groupKey, String type) {
+    public ReportTableDefinitionListVo(String id, String name, String groupKey, String type,String queryParams) {
         this.id = id;
         this.name = name;
         this.groupKey = groupKey;
         this.type = type;
+        this.queryParams = queryParams;
     }
 }
