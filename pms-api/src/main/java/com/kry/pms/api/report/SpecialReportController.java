3dc41c4baaad4ecdf0b2c88032a30350268f2638
Louis <louis_lw@126.com>
2020-05-22 17:05:19 +0800

    交班报表

---
+++ pms-api/src/main/java/com/kry/pms/api/report/SpecialReportController.java
@@ -0,0 +1,42 @@
+package com.kry.pms.api.report;
+
+import com.kry.pms.api.BaseController;
+import com.kry.pms.base.HttpResponse;
+import com.kry.pms.service.report.SpecialReportService;
+import com.kry.pms.service.report.impl.SpecialReportServiceImpl;
+import freemarker.template.TemplateException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/api/v1/report/special")
+public class SpecialReportController extends BaseController {
+    @Autowired
+    SpecialReportService specialReportService;
+
+    @GetMapping
+    @RequestMapping("/cost")
+    public HttpResponse<List<Map<String, Object>>> cost(@RequestParam(required = true) String employee_id, @RequestParam(required = true) String business_date, @RequestParam(required = true) String shift) throws IOException, TemplateException {
+        HttpResponse<List<Map<String, Object>>> rep = new HttpResponse<>();
+        LocalDate localDate = LocalDate.parse(business_date);
+        rep.addData(specialReportService.billCostStat(getCurrentHotleCode(), employee_id, shift, localDate));
+        return rep;
+    }
+
+    @GetMapping
+    @RequestMapping("/shift/pay")
+    public HttpResponse<List<Map<String, Object>>> pay(@RequestParam(required = true) String employee_id, @RequestParam(required = true) String business_date, @RequestParam(required = true) String shift) throws IOException, TemplateException {
+        HttpResponse<List<Map<String, Object>>> rep = new HttpResponse<>();
+        LocalDate localDate = LocalDate.parse(business_date);
+        rep.addData(specialReportService.billPayStat(getCurrentHotleCode(), employee_id, shift, localDate));
+        return rep;
+    }
+}
---
+++ pms-service/src/main/java/com/kry/pms/service/report/SpecialReportService.java
@@ -0,0 +1,15 @@
+package com.kry.pms.service.report;
+
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public interface SpecialReportService {
+
+    List<Map<String, Object>> billCostStat(String hotelCode, String employeeId, String shift, LocalDate businessDate) throws IOException, TemplateException;
+
+    List<Map<String, Object>> billPayStat(String hotelCode, String employeeId, String shift, LocalDate businessDate) throws IOException, TemplateException;
+}
