027ed0a597277e12ede22eed527f344296c6a091
huanghaibin <631131196@qq.com>
2020-06-24 17:43:50 +0800

    添加登录者ip进session

--- pms-auth/src/main/java/com/kry/pms/utils/ShiroUtils.java
+++ pms-auth/src/main/java/com/kry/pms/utils/ShiroUtils.java
@@ -11,26 +11,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.kry.pms.model.persistence.sys.User;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class ShiroUtils {
-	
-	@Autowired
+
+    @Autowired
     private static SessionDAO sessionDAO;
 
     public static Subject getSubjct() {
-    	Subject subject = SecurityUtils.getSubject();
+        Subject subject = SecurityUtils.getSubject();
         return subject;
     }
+
     public static User getUser() {
         Object object = getSubjct().getPrincipal();
-        return (User)object;
+        return (User) object;
     }
+
     public static String getUserId() {
-    	User user = getUser();
-    	if(user != null) {
-    		return getUser().getId();
-    	}
-    	return null;
+        User user = getUser();
+        if (user != null) {
+            return getUser().getId();
+        }
+        return null;
     }
+
     public static void logout() {
         getSubjct().logout();
     }
@@ -40,4 +45,44 @@ public class ShiroUtils {
 //        Collection<Session> sessions = sessionDAO.getActiveSessions();
         return principals;
     }
+
+    //获取请求者的ip
+    public static String getIpAdrress(HttpServletRequest request) {
+        String ip = null;
+
+        //X-Forwarded-For：Squid 服务代理
+        String ipAddresses = request.getHeader("X-Forwarded-For");
+        String unknown = "unknown";
+        if (ipAddresses == null || ipAddresses.length() == 0 || unknown.equalsIgnoreCase(ipAddresses)) {
+            //Proxy-Client-IP：apache 服务代理
+            ipAddresses = request.getHeader("Proxy-Client-IP");
+        }
+
+        if (ipAddresses == null || ipAddresses.length() == 0 || unknown.equalsIgnoreCase(ipAddresses)) {
+            //WL-Proxy-Client-IP：weblogic 服务代理
+            ipAddresses = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if (ipAddresses == null || ipAddresses.length() == 0 || unknown.equalsIgnoreCase(ipAddresses)) {
+            //HTTP_CLIENT_IP：有些代理服务器
+            ipAddresses = request.getHeader("HTTP_CLIENT_IP");
+        }
+
+        if (ipAddresses == null || ipAddresses.length() == 0 || unknown.equalsIgnoreCase(ipAddresses)) {
+            //X-Real-IP：nginx服务代理
+            ipAddresses = request.getHeader("X-Real-IP");
+        }
+
+        //有些网络通过多层代理，那么获取到的ip就会有多个，一般都是通过逗号（,）分割开来，并且第一个ip为客户端的真实IP
+        if (ipAddresses != null && ipAddresses.length() != 0) {
+            ip = ipAddresses.split(",")[0];
+        }
+
+        //还是不能获取到，最后再通过request.getRemoteAddr();获取
+        if (ip == null || ip.length() == 0 || unknown.equalsIgnoreCase(ipAddresses)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
 }
