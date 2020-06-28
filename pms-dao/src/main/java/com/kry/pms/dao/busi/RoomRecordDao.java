0f632d8cdb5275eef94c76b1512e33edbf7e0c36
Louis <louis_lw@126.com>
2020-06-28 11:21:44 +0800

    晚上删除房间记录

--- pms-dao/src/main/java/com/kry/pms/dao/busi/RoomRecordDao.java
+++ pms-dao/src/main/java/com/kry/pms/dao/busi/RoomRecordDao.java
@@ -11,75 +11,77 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface RoomRecordDao extends BaseDao<RoomRecord>{
+public interface RoomRecordDao extends BaseDao<RoomRecord> {
 
-	List<RoomRecord> findByHotelCodeAndRecordDate(String hotelCode, LocalDate recordDate);
+    List<RoomRecord> findByHotelCodeAndRecordDate(String hotelCode, LocalDate recordDate);
 
-	List<RoomRecord> findByHotelCodeAndCheckInRecord(String hotelCode, CheckInRecord checkInRecord);
-	List<RoomRecord> findByCheckInRecord(CheckInRecord checkInRecord);
+    List<RoomRecord> findByHotelCodeAndCheckInRecord(String hotelCode, CheckInRecord checkInRecord);
 
-	@Query(nativeQuery = true, value = " select DATE_FORMAT(tr.record_date, '%Y-%m-%d') record_date, tr.room_price \n" +
-			" from t_room_record tr where 1=1 " +
-			" and if(:recordDate is not null && :recordDate != '', DATE_FORMAT(tr.record_date, '%Y-%m-%d')=:recordDate, 1=1 ) " +
-			" and if(:recordId is not null && :recordId != '', tr.check_in_record_id=:recordId, 1=1 ) ")
-	Map<String, Object> recordDateAndRoomPrice(@Param("recordDate") String recordDate, @Param("recordId") String recordId);
+    List<RoomRecord> findByCheckInRecord(CheckInRecord checkInRecord);
 
-	@Query(nativeQuery = true, value = " select * from t_room_record \n" +
-			" where record_date >= :recordDate and check_in_record_id = :checkInRecordId ")
-	List<RoomRecord> recordDateAndCheckInRecord(@Param("recordDate") LocalDate recordDate, @Param("checkInRecordId") String checkInRecordId);
+    @Query(nativeQuery = true, value = " select DATE_FORMAT(tr.record_date, '%Y-%m-%d') record_date, tr.room_price \n" +
+            " from t_room_record tr where 1=1 " +
+            " and if(:recordDate is not null && :recordDate != '', DATE_FORMAT(tr.record_date, '%Y-%m-%d')=:recordDate, 1=1 ) " +
+            " and if(:recordId is not null && :recordId != '', tr.check_in_record_id=:recordId, 1=1 ) ")
+    Map<String, Object> recordDateAndRoomPrice(@Param("recordDate") String recordDate, @Param("recordId") String recordId);
 
-	@Modifying
-	@Query(value = " update t_room_record set is_account_entry = ?1 where id = ?2 ",nativeQuery = true)
-	int updateIsAccountEntry(String isAccountEntry, String id);
+    @Query(nativeQuery = true, value = " select * from t_room_record \n" +
+            " where record_date >= :recordDate and check_in_record_id = :checkInRecordId ")
+    List<RoomRecord> recordDateAndCheckInRecord(@Param("recordDate") LocalDate recordDate, @Param("checkInRecordId") String checkInRecordId);
 
-	@Query(nativeQuery = true, value = " select \n" +
-			" trr.id, \n" +
-			" tcr.id cirId, \n" +
-			" tgr.room_num roomNum, \n" +
-			" main.account_id mainAccountId, \n" +
-			" tsm.id setMealId, \n" +
-			" tsm.product_id productId, \n" +
-			" tsm.account_id setMealAccountId, \n" +
-			" tcr.account_id cirAccountId, \n" +
-			" trr.cost, \n" +
-			" tcr.hotel_code hotelCode, \n" +
-			" tsm.total setMealCost \n" +
-			" from t_room_record trr \n" +
-			"  left join t_checkin_record tcr on trr.check_in_record_id = tcr.id \n" +
-			"  left join t_set_meal tsm on tcr.set_meal_id = tsm.id \n" +
-			"  left join t_checkin_record main on tcr.main_record_id = main.id \n" +
-			"  left join t_guest_room tgr on trr.guest_room_id = tgr.id \n" +
-			" where 1=1 and tcr.`status` = 'I' " +
-			"  and trr.record_date = ?1 \n" +
-			"  and trr.hotel_code = ?2 \n" +
-			"  and trr.is_account_entry = ?3 ")
-	List<Map<String, Object>> auditRoomRecord(LocalDate recordDate, String hotelCode, String isAccountEntry);
+    @Modifying
+    @Query(value = " update t_room_record set is_account_entry = ?1 where id = ?2 ", nativeQuery = true)
+    int updateIsAccountEntry(String isAccountEntry, String id);
 
-	@Query(nativeQuery = true, value = " select \n" +
-			" trr.id, \n" +
-			" tcr.id cirId, \n" +
-			" tgr.room_num roomNum, \n" +
-			" main.account_id mainAccountId, \n" +
-			" tsm.id setMealId, \n" +
-			" tsm.product_id productId, \n" +
-			" tsm.account_id setMealAccountId, \n" +
-			" tcr.account_id cirAccountId, \n" +
-			" trr.cost, \n" +
-			" tcr.hotel_code hotelCode, \n" +
-			" tsm.total setMealCost \n" +
-			" from t_room_record trr \n" +
-			"  left join t_checkin_record tcr on trr.check_in_record_id = tcr.id \n" +
-			"  left join t_set_meal tsm on tcr.set_meal_id = tsm.id \n" +
-			"  left join t_checkin_record main on tcr.main_record_id = main.id \n" +
-			"  left join t_guest_room tgr on trr.guest_room_id = tgr.id \n" +
-			" where 1=1 and tcr.`status` = ?1 " +
-			"  and trr.record_date = ?2 \n" +
-			"  and trr.check_in_record_id = ?3 \n" +
-			"  and trr.hotel_code = ?4 \n" +
-			"  and trr.is_account_entry = ?5 ")
-	List<Map<String, Object>> checkInAuditRoomRecord(String status, LocalDate recordDate, String checkInId, String hotelCode, String isAccountEntry);
+    @Query(nativeQuery = true, value = " select \n" +
+            " trr.id, \n" +
+            " tcr.id cirId, \n" +
+            " tgr.room_num roomNum, \n" +
+            " main.account_id mainAccountId, \n" +
+            " tsm.id setMealId, \n" +
+            " tsm.product_id productId, \n" +
+            " tsm.account_id setMealAccountId, \n" +
+            " tcr.account_id cirAccountId, \n" +
+            " trr.cost, \n" +
+            " tcr.hotel_code hotelCode, \n" +
+            " tsm.total setMealCost \n" +
+            " from t_room_record trr \n" +
+            "  left join t_checkin_record tcr on trr.check_in_record_id = tcr.id \n" +
+            "  left join t_set_meal tsm on tcr.set_meal_id = tsm.id \n" +
+            "  left join t_checkin_record main on tcr.main_record_id = main.id \n" +
+            "  left join t_guest_room tgr on trr.guest_room_id = tgr.id \n" +
+            " where 1=1 and tcr.`status` = 'I' " +
+            "  and trr.record_date = ?1 \n" +
+            "  and trr.hotel_code = ?2 \n" +
+            "  and trr.is_account_entry = ?3 ")
+    List<Map<String, Object>> auditRoomRecord(LocalDate recordDate, String hotelCode, String isAccountEntry);
 
-	@Query(nativeQuery = true, value = " DELETE from t_room_record trr where trr.check_in_record_id = ?1 and trr.record_date >= ?2 and trr.is_account_entry = 'NO' ")
-	void deletedRecordAfter(String checkInRecordId, LocalDate recordDate);
+    @Query(nativeQuery = true, value = " select \n" +
+            " trr.id, \n" +
+            " tcr.id cirId, \n" +
+            " tgr.room_num roomNum, \n" +
+            " main.account_id mainAccountId, \n" +
+            " tsm.id setMealId, \n" +
+            " tsm.product_id productId, \n" +
+            " tsm.account_id setMealAccountId, \n" +
+            " tcr.account_id cirAccountId, \n" +
+            " trr.cost, \n" +
+            " tcr.hotel_code hotelCode, \n" +
+            " tsm.total setMealCost \n" +
+            " from t_room_record trr \n" +
+            "  left join t_checkin_record tcr on trr.check_in_record_id = tcr.id \n" +
+            "  left join t_set_meal tsm on tcr.set_meal_id = tsm.id \n" +
+            "  left join t_checkin_record main on tcr.main_record_id = main.id \n" +
+            "  left join t_guest_room tgr on trr.guest_room_id = tgr.id \n" +
+            " where 1=1 and tcr.`status` = ?1 " +
+            "  and trr.record_date = ?2 \n" +
+            "  and trr.check_in_record_id = ?3 \n" +
+            "  and trr.hotel_code = ?4 \n" +
+            "  and trr.is_account_entry = ?5 ")
+    List<Map<String, Object>> checkInAuditRoomRecord(String status, LocalDate recordDate, String checkInId, String hotelCode, String isAccountEntry);
+
+    @Modifying
+    @Query(nativeQuery = true, value = " DELETE from t_room_record trr where trr.check_in_record_id = ?1 and trr.record_date >= ?2 and trr.is_account_entry = 'NO' ")
+    void deletedRecordAfter(String checkInRecordId, LocalDate recordDate);
 
 }
